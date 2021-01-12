98af1135275c86cefd2fc110ac61793a92ad9806
ahussain15 <67604653+ahussain15@users.noreply.github.com>
2021-01-11 22:25:37 -0500

    ARLand

--- ARLand/app/src/main/java/com/hussainaaliya/arland/MainActivity.java
+++ ARLand/app/src/main/java/com/hussainaaliya/arland/MainActivity.java
@@ -2,13 +2,51 @@ package com.hussainaaliya.arland;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.ar.core.HitResult;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArFragment frag;
+    ModelRenderable rend;
+    Uri selected;
+
     @Override
+    @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        frag = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+
+    }
+
+    public static boolean checkSupport(Activity activity){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+
+            String openGlVersion = ((ActivityManager) Objects.requireNonNull(activity.getSystemService(Context.ACTIVITY_SERVICE))).getDeviceConfigurationInfo().getGlEsVersion();
+
+            if (Double.parseDouble(openGlVersion) >= 3.0) {
+                return true;
+            } else {
+                Toast.makeText(activity, "OpenGl Version Not Compatible", Toast.LENGTH_SHORT).show();
+                activity.finish();
+                return false;
+            }
+        } else {
+            Toast.makeText(activity, "Build Version Not Compatible", Toast.LENGTH_SHORT).show();
+            activity.finish();
+            return false;
+        }
     }
 }
---
+++ ARLand/app/src/main/java/com/hussainaaliya/arland/Model.java
@@ -0,0 +1,4 @@
+package com.hussainaaliya.arland;
+
+public class Model {
+}
