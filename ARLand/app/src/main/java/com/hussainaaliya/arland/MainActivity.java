4f4ab33220aad3250f382f702e42b00d3c4ec0e8
ahussain15 <67604653+ahussain15@users.noreply.github.com>
2021-02-08 11:27:36 -0500

    ARLand

--- ARLand/app/src/main/java/com/hussainaaliya/arland/MainActivity.java
+++ ARLand/app/src/main/java/com/hussainaaliya/arland/MainActivity.java
@@ -1,34 +1,93 @@
 package com.hussainaaliya.arland;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.app.ActivityManager;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
 import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
 import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
 
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Objects;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     ArFragment frag;
-    ModelRenderable rend;
-    Uri selected;
+    Spinner spinner;
 
+    ArrayList<String> options = new ArrayList<>(Arrays.asList("grammophone", "flowerpot", "pikachu", "telescope", "pizza"));
+    ArrayList<String> paths = new ArrayList<>(Arrays.asList("grammophone/model.gltf", "flowerpot/flowerpot.gltf", "pikachu/model.gltf", "telescope/model.gltf", "pizza/model.gltf"));
+
+    String cur_option = "";
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(!checkSupport(this))
+            return;
         setContentView(R.layout.activity_main);
+        spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, options);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
         frag = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+        frag.setOnTapArPlaneListener(
+                (HitResult hitresult, Plane plane, MotionEvent motionevent) -> {
+                    if (plane.getType() != Plane.Type.HORIZONTAL_UPWARD_FACING)
+                        return;
+                    Anchor anchor = hitresult.createAnchor();
+                    placeObject(frag, anchor, Uri.parse(cur_option));
+                }
+        );
+    }
 
+    private void placeObject(ArFragment fragment, Anchor anchor, Uri uri) {
+        ModelRenderable.builder()
+                .setSource(fragment.getContext(), uri)
+                .setIsFilamentGltf(true)
+                .build()
+                .thenAccept(modelRenderable -> addNodeToScene(fragment, anchor, modelRenderable))
+                .exceptionally(throwable -> {
+                            Toast.makeText(fragment.getContext(), "Something is not right:" + throwable.getMessage(), Toast.LENGTH_LONG).show();
+                            return null;
+                        }
+
+                );
+
+    }
+
+    private void addNodeToScene(ArFragment fragment, Anchor anchor, Renderable renderable) {
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode node = new TransformableNode(fragment.getTransformationSystem());
+        node.setRenderable(renderable);
+        node.setParent(anchorNode);
+        fragment.getArSceneView().getScene().addChild(anchorNode);
+        node.select();
     }
 
     public static boolean checkSupport(Activity activity){
@@ -48,5 +107,15 @@ public class MainActivity extends AppCompatActivity {
             activity.finish();
             return false;
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        cur_option = paths.get(i);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        cur_option = paths.get(0);
     }
 }
--- ARLand/app/src/main/java/com/hussainaaliya/arland/Model.java
+++ ARLand/app/src/main/java/com/hussainaaliya/arland/Model.java
@@ -1,4 +1,27 @@
 package com.hussainaaliya.arland;
 
+import com.google.ar.core.Plane;
+
 public class Model {
+    private int res;
+    private String display_name;
+    private Plane.Type plane_type;
+
+    public Model(int r, String dis, Plane.Type pt){
+        res = r;
+        display_name = dis;
+        plane_type = pt;
+    }
+
+    public Plane.Type getPlane_type() {
+        return plane_type;
+    }
+
+    public String getDisplay_name() {
+        return display_name;
+    }
+
+    public int getRes() {
+        return res;
+    }
 }
